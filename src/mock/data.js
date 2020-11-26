a1fb6a1fe8fa84ebffbd1e25e66620383e2df945
mmargauxx <margaux.laurier@gmail.com>
2020-11-26 16:30:47 +0100

    first commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,46 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'margaux.dev', // e.g: 'Name | Developer'
+  lang: 'nl', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: ' ',
+  name: 'Margaux Laurier',
+  subtitle: 'Creative Developer',
+  cta: '👩🏻‍💻',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Hi! Mijn naam is Margaux Laurier en ik omschrijf mezelf het liefst als een Creative Developer.',
+  paragraphTwo: 'Momenteel studeer ik Grafische en Digitale Media aan de AP Hogeschool, maar ik wist al snel dat mijn grootste interesse bij het coderen lag. Ik koos daarom ook dit jaar voor het keuzevak Advanced Front-end Coding en zette zo mijn eerste stappen in de React wereld. In de toekomst wil ik me graag toespitsen op custom built React Native websites en uitdagende projecten.',
+  paragraphThree: 'En ja, wat kan ik voor u betekenen? Heel veel als het van uw wensen afhangt! Ik bouw websites die er goed uitzien, goed werken én goed scoren in Google door rekening te houden met de nieuwste technologieën.',
+  resume: 'https://www.margaux.dev/docs/cv.pdf', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'madamemoustache.jpg',
+    title: 'Madame Moustache',
+    info: 'Wordpress Website met Google Analytics',
+    info2: 'Madame Moustache is een kapster aan huis. Ze maakt het haar klanten graag gemakkelijk en vroeg dus om een afspraaktool te integreren in de website, met e-mail meldingen voor zowel klant als kapster. Via Google Analytics is het mogelijk om de bezoekers van de website te leren kennen.',
+    url: 'https://madamemoustachebarber.be/',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'contacteer mij voor meer info',
+  btn: 'naar de mailbox!',
+  email: 'info@margaux.dev',
 };
 
 // FOOTER DATA
@@ -67,28 +49,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/margaux-laurier/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mmargauxx/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
--- src/pages/404.js
+++ src/pages/404.js
@@ -15,15 +15,15 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Page not found</title>
-        <html lang={lang || 'en'} />
+        <title>Pagina niet gevonden</title>
+        <html lang={lang || 'nl'} />
         <meta name="description" content="Page not found" />
       </Helmet>
       <section id="hero" className="jumbotron">
         <Container>
           <Fade bottom duration={1000} delay={500} distance="30px">
             <h1 className="hero-title text-center">
-              Sorry, this path does not exist{' '}
+              Sorry, deze pagina bestaat niet{' '}
               <span role="img" aria-label="emoji">
                 😞
               </span>
@@ -32,7 +32,7 @@ export default () => {
           <Fade bottom duration={1000} delay={1000} distance="30px">
             <p className="hero-cta justify-content-center">
               <Link className="cta-btn cta-btn--hero" to="/">
-                Go back
+                Ga terug
               </Link>
             </p>
           </Fade>
